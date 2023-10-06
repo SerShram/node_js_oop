80c3a7792fd77bd16fe697492110a92fce716eaa
sergey <sershram66@gmail.com>
2023-10-06 21:41:06 +0300

    Main route

--- index.js
+++ index.js
@@ -4,3 +4,25 @@ const qs = require('querystring');
 
 const {mimeTypes} = require('./utilities/mime');
 const {staticFile} = require('./utilities/static_file');
+
+const PORT = 3500;
+
+http.createServer(async function(request, response) {
+    const url = request.url;
+
+    switch(url) {
+        case '/':
+            console.log('main_page');
+            staticFile(response, '/html/main_page.html', '.html');
+            break;
+        default: 
+            const extname = String(path.extname(url)).toLocaleLowerCase();
+
+            if(extname in mimeTypes) {
+                staticFile(response, url, extname);
+            } else {
+                res.statusCode = 404;
+                res.end();
+            }
+    }
+}).listen(PORT);
