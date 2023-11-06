ef5f7ea6f29e4d681e75e9d5f5a7898834a4483e
sergey <sershram66@gmail.com>
2023-11-06 14:24:24 +0200

    Added reguser rout and class User

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const qs = require('querystring');
 
 const {mimeTypes} = require('./utilities/mime');
 const {staticFile} = require('./utilities/static_file');
+const User = require('./class/User');
 
 const PORT = 3500;
 
@@ -15,6 +16,41 @@ http.createServer(async function(request, response) {
             console.log('main_page');
             staticFile(response, '/html/main_page.html', '.html');
             break;
+            case '/reguser':
+                console.log('reguser');
+                if (request.method == 'POST') {
+                    let body = '';
+                    request.on('data', function (data) {
+                        body += data;
+                    });
+    
+                    request.on('end', async function () {
+                        let post = qs.parse(body);
+                        const user = new User(post.email, post.pass);
+                        if (!(await user.findUser())) {
+                            let result = await user.createUser();
+                            if (result) {
+                                response.end(JSON.stringify({
+                                    "success": true,
+                                    "action": "user was created"
+                                }))
+                            }
+                            else {
+                                response.end(JSON.stringify({
+                                    "success": false,
+                                    "action": "create user error"
+                                }))
+                            }
+                        }
+                        else {
+                            response.end(JSON.stringify({
+                                "success": false,
+                                "action": "user exists"
+                            }))
+                        }
+                    });
+                }
+                break;
         default: 
             const extname = String(path.extname(url)).toLocaleLowerCase();
 
